33c905125c7a5fec206bc248877cd04d31e907f2
Andrius <andrius.eagle@gmail.com>
2015-11-06 10:53:36 +0200

    casperjs remove line ending

--- casperTest.js
+++ casperTest.js
@@ -7,7 +7,7 @@
 
 var fs = require('fs');
 var utils = require('utils');
-var domain = fs.read('casperDomain.txt');
+var domain = fs.read('casperDomain.txt').replace(/(\r\n|\n|\r)/gm,"");
 utils.dump(domain);
 
 casper.test.begin('Testing hotel booking', 9, function suite(test) {
