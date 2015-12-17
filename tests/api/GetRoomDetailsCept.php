e99e3842265fffc36789629a65c3d66b840dd5da
Andrius <andrius.eagle@gmail.com>
2015-12-17 19:11:01 +0200

    test fix

--- tests/api/GetRoomDetailsCept.php
+++ tests/api/GetRoomDetailsCept.php
@@ -2,4 +2,4 @@
 $I = new ApiTester($scenario);
 $I->wantTo('Get list of hotels');
 $I->sendPOST('/hotels-api/rooms/2', []);
-$I->seeResponseContains('{"id":"2","title":"Vip","price":"180","image":"http:\/\/127.0.0.1:8000\/src\/Frontend\/Files\/rooms\/images\/source\/vip_ton.jpg","count":"2"}');
+$I->seeResponseContains('{"id":"2","title":"Vip","price":"180","image":"http:\/\/andriuss.ktu.ongr.rocks\/src\/Frontend\/Files\/rooms\/images\/source\/vip_ton.jpg","count":"2"}');
